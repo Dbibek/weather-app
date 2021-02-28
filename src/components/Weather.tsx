c3f56c1215a962dee6d6e6b4725fb4ba268e9091
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-02-28 21:34:27 +0200

    styling to weather data component appiled

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
 import { WeatherData } from "../store/types";
 
@@ -6,29 +6,40 @@ interface WeatherProps {
   data: WeatherData;
 }
 const Weather: FC<WeatherProps> = ({ data }) => {
+  const celsius = (data.main.temp_max - 273.15).toFixed(2);
   return (
     <section className="section">
       <div className="weather-container">
         <h1 className="title">
           {data.name}-{data.sys.country}
         </h1>
-        <div className="level">
-          <div className="level-item">
-            <div>
-              <p className="heading">{data.weather[0].description}</p>
-              <p className="imageicon">
-                <img
-                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-                  alt="weathericon"
-                />
-              </p>
-            </div>
-            <p>temperature:{data.main.temp_max}</p>
-            <p>humidity:{data.main.humidity}</p>
-            <p>wind-speed:{data.wind.speed}</p>
-            <p>sunrise:{data.sys.sunrise}</p>
-            <p>sunset:{data.sys.sunset}</p>
-            <p>{data.weather[0].main}</p>
+
+        <div className="level-item">
+          <div>
+            <p className="heading">{data.weather[0].description}</p>
+            <p className="imageicon">
+              <img
+                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                alt="weathericon"
+              />
+            </p>
+          </div>
+          <div className="level">
+            <p className="heading">temp</p>
+            <p className="title">{celsius} °C</p>
+          </div>
+          <div className="level">
+            <p className="heading">Humidity</p>
+            <p className="title">{data.main.humidity}</p>
+          </div>
+          <div className="level">
+            {" "}
+            <p className="heading">Wind</p>
+            <p className="title">{data.wind.speed}</p>
+          </div>
+          <div className="level">
+            <p className="heading">Pressure</p>
+            <p className="title">{data.main.pressure}</p>
           </div>
         </div>
       </div>
